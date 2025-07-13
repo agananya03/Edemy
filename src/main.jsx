7acbca9588102e9d21476267696e0cce8915237e
agananya03 <agananya54126@gmail.com>
2025-07-13 23:07:05 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AppContextprovider } from './context/AppContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <AppContextprovider>
+      <App />
+    </AppContextprovider>
+  </BrowserRouter>,
 )
