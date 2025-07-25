5ea6da966425fcf82907c1597da6ef1d1f1a819a
agananya03 <agananya54126@gmail.com>
2025-07-25 21:14:51 +0530

    add

--- src/components/student/TestimonialsSection.jsx
+++ src/components/student/TestimonialsSection.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { dummyTestimonial } from '../../assets/assets'
 
 const TestimonialsSection = () => {
   return (
-    <div>
-      <h1>Testimonials</h1>
+    <div className='pb-14 px-8 md:px-0'>
+      <h2 className='text-3xl font-medium text-gray-800'>Testimonials</h2>
+      <p className='md:text-base text-gray-500 mt-3'>Hear from our learners as they share their journeys of transformation, success, and how our <br />platform has made a differnce in their lives.</p>
+      <div>
+        {dummyTestimonial.map((testimonial, index)=>(
+          <div key={}>
+
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,23 +1,39 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { dummyCourses } from '../assets/assets';
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext(); // Make this private
 
 export const AppContextProvider = (props) => {
     const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
     const [allCourses, setAllcourses] = useState([]);
+    const [isEduactor, setIsEducator] = useState(true);
 
     const fetchAllcourses = async () => {
         setAllcourses(dummyCourses)
     }
 
+    const calculateRating = (course)=>{
+        if(course.courseRatings.length == 0){
+            return 0;
+        }
+        let totalRating = 0;
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+        return totalRating/ course.courseRatings.length
+    }
     useEffect(() => {
         fetchAllcourses()
     }, [])
 
     const value = {
         currency,
-        allCourses
+        allCourses,
+        navigate,
+        calculateRating,
+        isEduactor, setIsEducator
     }
 
     return (
