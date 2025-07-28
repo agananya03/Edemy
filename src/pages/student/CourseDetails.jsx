a2cb9056c74ddc86c217ecd9037a4c4b454a1edf
agananya03 <agananya54126@gmail.com>
2025-07-28 10:09:21 +0530

    edemy

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -8,7 +8,8 @@ import humanizeDuration from 'humanize-duration';
 const CourseDetails = () => {
   const {id} = useParams()
   const [courseData, setCourseData] = useState(null);
-  const {allCourses, calculateRating, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures} = useAppContext()
+  const [openSections, setOpenSections] = useState({});
+  const {allCourses, calculateRating, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures, currency} = useAppContext()
 
   const fetchCourseData = async () => {
     const findCourse = allCourses.find(course => course._id === id)
@@ -18,6 +19,14 @@ const CourseDetails = () => {
   useEffect(()=> {
     fetchCourseData()
   },[])
+
+  const toggleSection = (index)=>{
+    setOpenSections((prev)=>(
+      {...prev,
+        [index]: !prev[index]
+      }
+    ))
+  }
 
   return courseData ?(
     <>
@@ -49,14 +58,14 @@ const CourseDetails = () => {
             <div className='pt-5'>
               {courseData.courseContent.map((chapter, index) => (
                 <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
-                  <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'>
+                  <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={()=>toggleSection(index)}>
                     <div className='flex items-center gap-2'>
-                      <img src={assets.down_arrow_icon} alt="arrow icon" />
+                      <img className={`transform transition-transform ${openSections[index] ? 'rotate-180' : ''}`} src={assets.down_arrow_icon} alt="arrow icon" />
                       <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
                     </div>
                     <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
                   </div>
-                  <div className='overflow-hidden transition-all duration-300 max-h-96'>
+                  <div className={`overflow-hidden transition-all duration-300 ${openSections[index] ? 'max-h-96' : 'max-h-0'}`}>
                     <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-6 border-t border-gray-300'>
                       {chapter.chapterContent.map((lecture, i) => (
                         <li key={i} className='flex items-start gap-2 py-1'>
@@ -77,10 +86,23 @@ const CourseDetails = () => {
               ))}
             </div>
         </div>
+        <div className='py-20 text-sm md:text-default'>
+          <h3 className='text-xl font-semibold text-gray-800'>Course Description</h3>
+          <p className='pt-3 rich-text' dangerouslySetInnerHTML={{__html : courseData.courseDescription}}></p>
+        </div>
       </div>
       {/* right-column */}
-      <div>
-
+      <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]'>
+          <img src={courseData.courseThumbnail} alt="" />
+          <div className='p-5'>
+            <div className='flex items-center gap-2'>
+              <img className='w-3.5' src={assets.time_left_clock_icon} alt="time left clock icon" />
+              <p className='text-red-500'><span className='font-medium'>4 days</span> left at this price!</p>
+          </div>
+          <div>
+            <p>{currency}{(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2)}</p>
+          </div>
+        </div>
       </div>
     </div>
     </>
