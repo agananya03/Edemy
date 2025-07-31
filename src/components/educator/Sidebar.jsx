90a14230129ff331a14f3db95a08fc1cb8b269b3
agananya03 <agananya54126@gmail.com>
2025-07-31 13:02:57 +0530

    edemy

--- src/components/educator/Sidebar.jsx
+++ src/components/educator/Sidebar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import assets from '../../assets/assets'
+import { useAppContext } from '../../context/AppContext';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
-  return (
-    <div>
-      <h1>Sidebar</h1>
+
+  const {isEducator} = useAppContext()
+
+  const menuItems = [
+    {name: 'Dashboard', path: '/educator', icon: assets.home_icon},
+    {name: 'Add Course', path: '/educator/add-course', icon: assets.add_icon},
+    {name: 'My Courses', path: '/eduactor/my-courses', icon: assets.my_course_icon},
+    {name: 'Student Enrolled', path: '/eduactor/student-enrolled', icon: assets.person_tick_icon},
+  ];
+  return isEducator && (
+    <div className='md:w-64 w-16 border-r min-h-screen text-base border-gray-500 py-2 flex flex-col'>
+      {menuItems.map((item)=>{
+        <NavLink>
+          <img src={item.icon} alt="" className='w-6 h-6'/>
+          <p className='md:block hidden text-center'>{item.name}</p>
+        </NavLink>
+      })}
     </div>
   )
 }
--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educator/Navbar'
+import Sidebar from '../../components/educator/Sidebar'
 
 const Educator = () => {
   return (
-    <div>
-      <h1>Educator Page</h1>
-      <div>
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar/>
+      <div className='flex'>
+        <Sidebar/>
+        <div className='flex-1'>
         {<Outlet/>}
+        </div>
       </div>
     </div>
   )
