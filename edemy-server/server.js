3b52d0966de68500700f5b3f3a08ee51d36f9be8
agananya03 <agananya54126@gmail.com>
2025-08-16 09:20:59 +0530

    server

--- edemy-server/server.js
+++ edemy-server/server.js
@@ -1,1 +1,18 @@
 import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+
+const app = express()
+
+await connectDB();
+
+app.use(cors())
+
+app.get('/', (req, res) => res.send("API Working"))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on the port ${PORT}`)
+})
